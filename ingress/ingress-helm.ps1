5cb41177bdb8e7b0e18f96dad98706706695e29b
jeewansoori <g1soori@gmail.com>
2020-05-31 10:51:27 +0800

    First commit push content

---
+++ ingress/ingress-helm.ps1
@@ -0,0 +1,6 @@
+helm install nginx-ingress-hygieia stable/nginx-ingress \
+    --namespace ingress-basic \
+    -f internal-ingress.yaml \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
